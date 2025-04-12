637ec8365b5716130afdfd5927f917e50eda373c
ChamikaR04 <chamikaseekkubadu@gmail.com>
2025-04-12 12:06:50 +0530

    Add dog class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Dog o1 = new Dog("Shero", 8, "Brown");
+
+        String name = o1.getName();
+        System.out.println(name);
     }
 }
